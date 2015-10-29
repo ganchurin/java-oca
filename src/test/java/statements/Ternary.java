28d9f8b98d0ff1856ce0e5363f5cda5fd7050174
ganchurin <eugeny.ganchurin@gmail.com>
2015-10-30 02:38:57 +0300

    Testing ternary

--- src/test/java/statements/Ternary.java
+++ src/test/java/statements/Ternary.java
@@ -7,16 +7,20 @@ import static org.junit.Assert.assertTrue;
 public class Ternary {
 
 	@Test
-	public void testTernary() {
+	public void testTernaryLeftExpression() {
 		int x = 1, y = 1;
-		int r = x == y ? x++ : y++;
-		assertTrue(x == 2);
-		assertTrue(y == 1);
-		assertTrue(r == 1);
+		int r = x == y ? --x : --y;
+		assertTrue(x == 0);
+		assertTrue(y == 1); // stays unchanged
+		assertTrue(r == 0);
+	}
 
-		int q = x == y ? x-- : y--;
-		assertTrue(x == 2);
+	@Test
+	public void testTernaryRightExpression() {
+		int x = 2, y = 1;
+		int q = x == y ? --x : --y;
+		assertTrue(x == 2); // stays unchanged
 		assertTrue(y == 0);
-		assertTrue(q == 1);
+		assertTrue(q == 0);
 	}
 }
