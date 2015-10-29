816a68fc476ef3f1a110150d9e53996afaf40fdc
ganchurin <eugeny.ganchurin@gmail.com>
2015-10-30 02:45:41 +0300

    Testing base

--- src/test/java/building_blocks/base/IntBase.java
+++ src/test/java/building_blocks/base/IntBase.java
@@ -9,14 +9,14 @@ public class IntBase {
 	@Test
 	public void testBase() {
 		// Check octal base
-		assertTrue(07777 == 4095);    // 8 ^ 4 - 1
+		assertTrue(07777 == 4095); // 8 ^ 4 - 1
 
 		// Check binary base
-		assertTrue(0b1111 == 15);    // 2 ^ 4 - 1
+		assertTrue(0b1111 == 15); // 2 ^ 4 - 1
 		assertTrue(0B1111 == 15);
 
 		// Check hex base
-		assertTrue(0xFFFF == 65535);    // 16 ^ 4 - 1
+		assertTrue(0xFFFF == 65535); // 16 ^ 4 - 1
 		assertTrue(0xffff == 65535);
 		assertTrue(0XFFFF == 65535);
 		assertTrue(0Xffff == 65535);
