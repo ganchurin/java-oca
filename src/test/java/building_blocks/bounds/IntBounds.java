944e8fc5d8acd2a2b682bdab7c880629c0aa3a5d
ganchurin <eugeny.ganchurin@gmail.com>
2015-10-31 02:20:04 +0300

    Testing IntBounds

--- src/test/java/building_blocks/bounds/IntBounds.java
+++ src/test/java/building_blocks/bounds/IntBounds.java
@@ -8,11 +8,13 @@ public class IntBounds {
 
 	@Test
 	public void testUpperBound() {
+		assertTrue(Integer.MAX_VALUE == 2_147_483_647);
 		assertTrue(Integer.MAX_VALUE + 1 == Integer.MIN_VALUE);
 	}
 
 	@Test
 	public void testLowerBound() {
+		assertTrue(Integer.MIN_VALUE == -2_147_483_648);
 		assertTrue(Integer.MIN_VALUE - 1 == Integer.MAX_VALUE);
 	}
 }
