1f6615590267c5b4ae885ce5425ba20ea2963e35
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-02 02:29:09 +0300

    Reformatted code

--- src/test/java/operators/binary/arithmetic/Modulus.java
+++ src/test/java/operators/binary/arithmetic/Modulus.java
@@ -1,16 +1,17 @@
 package operators.binary.arithmetic;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class Modulus {
 
 	@Test
 	public void testModulus() {
-		Assert.assertTrue(1 % 3 == 1);
-		Assert.assertTrue(2 % 3 == 2);
-		Assert.assertTrue(3 % 3 == 0);
-		Assert.assertTrue(4 % 3 == 1);
-		Assert.assertTrue(5 % 3 == 2);
+		assertTrue(1 % 3 == 1);
+		assertTrue(2 % 3 == 2);
+		assertTrue(3 % 3 == 0);
+		assertTrue(4 % 3 == 1);
+		assertTrue(5 % 3 == 2);
 	}
 }
