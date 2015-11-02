8f29b77a297a2bf16662e27b8ec3840d48a9f34c
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-03 00:24:03 +0300

    Introduced tests for string concatenation

--- src/test/java/core/string/StringOps.java
+++ src/test/java/core/string/StringOps.java
@@ -7,15 +7,31 @@ import static org.junit.Assert.assertEquals;
 public class StringOps {
 
 	@Test
-	public void testConcatenation() {
+	public void concatenateWithPlusOp() {
 		assertEquals("ab", "a" + "b");
 		assertEquals("ab1", "a" + "b" + 1);
 		assertEquals("ab11", "a" + "b" + 1 + 1);
 		assertEquals("1ab", 1 + "a" + "b");
 		assertEquals("2ab", 1 + 1 + "a" + "b");
+	}
 
+	@Test
+	public void concatenateWithCompoundOp() {
 		String s = "abc";
-		assertEquals("abcd", s += "d");
-		assertEquals("abcd1", s += 1);
+		assertEquals("abc", s);
+		s += "d";
+		assertEquals("abcd", s);
+		s += 1;
+		assertEquals("abcd1", s);
+	}
+
+	@Test
+	public void concatenateWithConcatMethod() {
+		String s = "abc";
+		assertEquals("abc", s);
+		s = s.concat("d");
+		assertEquals("abcd", s);
+		s.concat("e");
+		assertEquals("abcd", s);
 	}
 }
