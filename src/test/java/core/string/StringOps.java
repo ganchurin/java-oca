a9269a5c23fea9de7392d710cfeef29188324fe4
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-03 01:03:51 +0300

    Introduced tests for string pool

--- src/test/java/core/string/StringOps.java
+++ src/test/java/core/string/StringOps.java
@@ -36,7 +36,7 @@ public class StringOps {
 	}
 
 	@Test
-	public void takeStringFromPool() {
+	public void checkReferenceEquality() {
 		String s1 = "Firefly";
 		String s2 = "Firefly";
 		assertTrue(s1 == s2);
@@ -49,5 +49,21 @@ public class StringOps {
 		assertFalse(s1 == s4);
 		assertFalse(s2 == s4);
 		assertFalse(s3 == s4);
+	}
+
+	@Test
+	public void checkValueEquality() {
+		String s1 = "Firefly";
+		String s2 = "Firefly";
+		assertEquals(s1, s2);
+
+		String s3 = new String("Firefly");
+		assertEquals(s1, s3);
+		assertEquals(s2, s3);
+
+		String s4 = new String(s1);
+		assertEquals(s1, s4);
+		assertEquals(s2, s4);
+		assertEquals(s3, s4);
 	}
 }
