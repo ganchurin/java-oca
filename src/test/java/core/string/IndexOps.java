176903c952bd0e0c7240e497276d349843131c31
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-03 01:57:00 +0300

    Introduced tests for 'String.substring'

--- src/test/java/core/string/IndexOps.java
+++ src/test/java/core/string/IndexOps.java
@@ -2,6 +2,7 @@ package core.string;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class IndexOps {
@@ -46,5 +47,23 @@ public class IndexOps {
 		assertTrue(numbers.indexOf("12", 1) == -1);
 		assertTrue(numbers.indexOf("23", 1) == 1);
 		assertTrue(numbers.indexOf("34", 1) == -1);
+	}
+
+	@Test
+	public void checkSubstring() {
+		String numbers = "123";
+		assertEquals(numbers.substring(0), "123");
+		assertEquals(numbers.substring(1), "23");
+		assertEquals(numbers.substring(2), "3");
+		assertEquals(numbers.substring(3), "");
+	}
+
+	@Test
+	public void checkSubstringUntilIndex() {
+		String numbers = "123";
+		assertEquals(numbers.substring(0, 3), "123");
+		assertEquals(numbers.substring(0, 2), "12");
+		assertEquals(numbers.substring(0, 1), "1");
+		assertEquals(numbers.substring(0, 0), "");
 	}
 }
