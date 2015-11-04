590fdbed678e7573c2da6e36824f94fb858b06e0
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-04 13:33:55 +0300

    Introduced tests for 'String.toLowerCase' and 'String.toUpperCase'

--- src/test/java/core/string/StringOps.java
+++ src/test/java/core/string/StringOps.java
@@ -2,7 +2,8 @@ package core.string;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StringOps {
 
@@ -10,6 +11,16 @@ public class StringOps {
 	public void checkLength() {
 		String numbers = "12345";
 		assertTrue(numbers.length() == 5);
+	}
+
+	@Test
+	public void checkLowerCase() {
+		assertEquals("aBc123".toLowerCase(), "abc123");
+	}
+
+	@Test
+	public void checkUpperCase() {
+		assertEquals("aBc123".toUpperCase(), "ABC123");
 	}
 
 }
