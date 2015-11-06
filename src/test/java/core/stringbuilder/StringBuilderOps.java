911de65c0764c5a8eb82b10a753d93befc9f2a18
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-07 02:40:54 +0300

    Introduced tests for StringBuilder

--- src/test/java/core/stringbuilder/StringBuilderOps.java
+++ src/test/java/core/stringbuilder/StringBuilderOps.java
@@ -2,6 +2,7 @@ package core.stringbuilder;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class StringBuilderOps {
@@ -10,5 +11,23 @@ public class StringBuilderOps {
 	public void checkLength() {
 		StringBuilder sb = new StringBuilder("Firefly");
 		assertTrue(sb.length() == 7);
+	}
+
+	@Test
+	public void checkSubstring() {
+		StringBuilder sb = new StringBuilder("123");
+		assertEquals(sb.substring(0), "123");
+		assertEquals(sb.substring(1), "23");
+		assertEquals(sb.substring(2), "3");
+		assertEquals(sb.substring(3), "");
+	}
+
+	@Test
+	public void checkSubstringUntilIndex() {
+		StringBuilder sb = new StringBuilder("123");
+		assertEquals(sb.substring(0, 3), "123");
+		assertEquals(sb.substring(0, 2), "12");
+		assertEquals(sb.substring(0, 1), "1");
+		assertEquals(sb.substring(0, 0), "");
 	}
 }
