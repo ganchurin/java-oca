586ce486033ac5870dae9914c864ef9edcaccce3
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-07 02:51:19 +0300

    Introduced tests for StringBuilder

--- src/test/java/core/stringbuilder/StringBuilderOps.java
+++ src/test/java/core/stringbuilder/StringBuilderOps.java
@@ -1,8 +1,8 @@
 package core.stringbuilder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class StringBuilderOps {
@@ -11,5 +11,19 @@ public class StringBuilderOps {
 	public void checkLength() {
 		StringBuilder sb = new StringBuilder("Firefly");
 		assertTrue(sb.length() == 7);
+	}
+
+	@Test
+	public void checkAppend() {
+		StringBuilder sb = new StringBuilder("StringBuilder");
+		sb.append("Is").append("Mutable");
+		Assert.assertEquals(sb.toString(), "StringBuilderIsMutable");
+	}
+
+	@Test
+	public void checkInsert() {
+		StringBuilder sb = new StringBuilder("StringBuilderMutable");
+		sb.insert(13, "Is");
+		Assert.assertEquals(sb.toString(), "StringBuilderIsMutable");
 	}
 }
