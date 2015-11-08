984f4eccd7e16dbc8579fef21beca6adbae40ff5
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-09 02:54:47 +0300

    Introduced tests for array

--- src/test/java/core/array/NewArray.java
+++ src/test/java/core/array/NewArray.java
@@ -7,12 +7,30 @@ import static org.junit.Assert.*;
 public class NewArray {
 
 	@Test
-	public void createArray() {
-		int[] a = {1, 2, 3, 4, 5};
-		int[] b = {1, 2, 3, 4, 5};
-		assertNotEquals(a, b);
-		int[] c = b;
-		assertEquals(b, c);
+	public void checkArrayEquality() {
+		int[] array1 = {1, 2, 3, 4, 5};
+		int[] array2 = {1, 2, 3, 4, 5};
+		// Array object does not override 'equals'
+		// Comparison by default checks references which are different for any two objects
+		assertFalse(array1.equals(array2));
+		int[] array3 = array2;
+		// Variable 'c' refers to the same object as variable 'b'
+		assertTrue(array2.equals(array3));
+		// Use 'Arrays.equals' static method to compare arrays by values
+	}
+
+	@Test
+	public void createOneDimensionalArray() {
+		// You can use brackets after array type or after variable name - it doesn't matter
+		int[] array1 = {1, 2, 3};
+		int array2[] = {1, 2, 3};
+		// 'Assert.assertArrayEquals' uses 'Arrays.equals'
+		assertArrayEquals(array1, array2);
+		int[] array3 = new int[3];
+		array3[0] = 1;
+		array3[1] = 2;
+		array3[2] = 3;
+		assertArrayEquals(array1, array3);
 	}
 
 	@Test
