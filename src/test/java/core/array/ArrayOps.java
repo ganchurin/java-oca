f48bb35d0d094faf53fc60279a372dc703598c69
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-09 02:20:14 +0300

    Introduced tests for array

--- src/test/java/core/array/ArrayOps.java
+++ src/test/java/core/array/ArrayOps.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ArrayOps {
 
@@ -22,5 +23,20 @@ public class ArrayOps {
 		String[] exp = {"10", "20", "3"};
 		Arrays.sort(arr);
 		assertArrayEquals(arr, exp);
+	}
+
+	@Test
+	public void searchInt() {
+		int[] arr = {1, 10, 100, 1000};
+		assertTrue(Arrays.binarySearch(arr, 1) == 0);
+		assertTrue(Arrays.binarySearch(arr, 10) == 1);
+		assertTrue(Arrays.binarySearch(arr, 100) == 2);
+		assertTrue(Arrays.binarySearch(arr, 1000) == 3);
+
+		assertTrue(Arrays.binarySearch(arr, 0) == -1);
+		assertTrue(Arrays.binarySearch(arr, 2) == -2);
+		assertTrue(Arrays.binarySearch(arr, 20) == -3);
+		assertTrue(Arrays.binarySearch(arr, 200) == -4);
+		assertTrue(Arrays.binarySearch(arr, 2000) == -5);
 	}
 }
