2de2e2e5dd8796c564d6a1ad6632649fa3109a49
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-10 01:53:19 +0300

    Introduced tests for ArrayList

---
+++ src/test/java/core/arraylist/ArrayListOps.java
@@ -0,0 +1,65 @@
+package core.arraylist;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class ArrayListOps {
+
+	@Test
+	public void addElement() {
+		List<String> stocks = new ArrayList<>();
+
+		stocks.add("AAPL");
+		assertEquals(stocks, asList("AAPL"));
+
+		stocks.add(0, "GOOG");
+		assertEquals(stocks, asList("GOOG", "AAPL"));
+
+		stocks.add(2, "IBM");
+		assertEquals(stocks, asList("GOOG", "AAPL", "IBM"));
+
+		// throws IndexOutOfBoundsException
+		// stocks.add(4, "IBM");
+	}
+
+	@Test
+	public void setElement() {
+		List<String> stocks = new ArrayList<>();
+
+		stocks.add("AAPL");
+		assertEquals(stocks, asList("AAPL"));
+
+		String prev = stocks.set(0, "YNDX");
+		assertEquals(prev, "AAPL");
+		assertEquals(stocks, asList("YNDX"));
+
+		// throws IndexOutOfBoundsException
+		// stocks.set(1, "IBM");
+	}
+
+	@Test
+	public void removeElement() {
+		List<String> stocks = new ArrayList<>();
+		stocks.add("AAPL");
+		stocks.add("YNDX");
+		stocks.add("AAPL");
+		assertEquals(stocks, asList("AAPL", "YNDX", "AAPL"));
+
+		// Provide an object to remove it from list
+		// The first matched element will be removed
+		stocks.remove("AAPL");
+		assertEquals(stocks, asList("YNDX", "AAPL"));
+
+		// Provide an integer to remove element by index
+		stocks.remove(0);
+		assertEquals(stocks, asList("AAPL"));
+
+		// throws IndexOutOfBoundsException
+		// stocks.remove(1);
+	}
+}
