f7a6bbdb83911e4c6445abef3435042c4c3f60eb
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-12 02:43:30 +0300

    Introduced tests for LocalDate

--- src/test/java/core/time/LocalDateTest.java
+++ src/test/java/core/time/LocalDateTest.java
@@ -10,13 +10,13 @@ import static org.junit.Assert.assertEquals;
 public class LocalDateTest {
 
 	@Test
-	public void createCurrentTime() {
+	public void createCurrentDate() {
 		// Use static method 'now' to create new date object with current time
 		LocalDate date = LocalDate.now();
 	}
 
 	@Test
-	public void createCustomTime() {
+	public void createCustomDate() {
 		// Use static methods 'of' to create custom date objects
 		// Set year, month index and day of month
 		LocalDate d1 = LocalDate.of(2015, 10, 30);
@@ -29,5 +29,23 @@ public class LocalDateTest {
 		assertEquals(d2.getYear(), 2015);
 		assertEquals(d2.getMonth(), Month.OCTOBER);
 		assertEquals(d2.getDayOfMonth(), 30);
+	}
+
+	@Test
+	public void checkPlusDate() {
+		LocalDate d = LocalDate.of(2015, 10, 30);
+		assertEquals(d.plusYears(1), LocalDate.of(2016, 10, 30));
+		assertEquals(d.plusMonths(2), LocalDate.of(2015, 12, 30));
+		assertEquals(d.plusWeeks(3), LocalDate.of(2015, 11, 20));
+		assertEquals(d.plusDays(4), LocalDate.of(2015, 11, 3));
+	}
+
+	@Test
+	public void checkMinusDate() {
+		LocalDate d = LocalDate.of(2015, 10, 30);
+		assertEquals(d.minusYears(1), LocalDate.of(2014, 10, 30));
+		assertEquals(d.minusMonths(2), LocalDate.of(2015, 8, 30));
+		assertEquals(d.minusWeeks(3), LocalDate.of(2015, 10, 9));
+		assertEquals(d.minusDays(4), LocalDate.of(2015, 10, 26));
 	}
 }
