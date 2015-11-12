7a5727961fb7aff68a3a02d6cf4bceb41e72215d
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-13 02:39:17 +0300

    Introduced tests for DateTimeFormatter

--- src/test/java/core/time/DateTimeFormatterTest.java
+++ src/test/java/core/time/DateTimeFormatterTest.java
@@ -15,6 +15,7 @@ public class DateTimeFormatterTest {
 	public void formatDate() {
 		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
 		LocalDate date = LocalDate.of(2015, 10, 30);
+		// Format date from formatter or from date
 		assertEquals(formatter.format(date), "2015-10-30");
 		assertEquals(date.format(formatter), "2015-10-30");
 	}
@@ -23,6 +24,7 @@ public class DateTimeFormatterTest {
 	public void formatTime() {
 		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
 		LocalTime time = LocalTime.of(10, 30, 15, 1000);
+		// Format time from formatter or from time
 		assertEquals(formatter.format(time), "10:30:15.000001");
 		assertEquals(time.format(formatter), "10:30:15.000001");
 	}
@@ -31,6 +33,7 @@ public class DateTimeFormatterTest {
 	public void formatDateTime() {
 		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 		LocalDateTime dateTime = LocalDateTime.of(2015, 10, 30, 10, 30, 15, 1000);
+		// Format dateTime from formatter or from dateTime
 		assertEquals(formatter.format(dateTime), "2015-10-30T10:30:15.000001");
 		assertEquals(dateTime.format(formatter), "2015-10-30T10:30:15.000001");
 	}
