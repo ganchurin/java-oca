4e0a36032dfade3c05874ba2215172fdf1b6377e
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-13 02:33:09 +0300

    Introduced tests for Period

---
+++ src/test/java/core/time/PeriodTest.java
@@ -0,0 +1,35 @@
+package core.time;
+
+import org.junit.Test;
+
+import java.time.Period;
+
+import static org.junit.Assert.assertEquals;
+
+public class PeriodTest {
+
+	@Test
+	public void createCustomPeriod() {
+		// Use static methods to create new period object
+		Period p1 = Period.ofYears(10);
+		checkPeriod(p1, 10, 0, 0);
+
+		Period p2 = Period.ofMonths(15);
+		checkPeriod(p2, 0, 15, 0);
+
+		Period p3 = Period.ofWeeks(12);
+		checkPeriod(p3, 0, 0, 84);
+
+		Period p4 = Period.ofDays(100);
+		checkPeriod(p4, 0, 0, 100);
+
+		Period p5 = Period.of(10, 15, 100);
+		checkPeriod(p5, 10, 15, 100);
+	}
+
+	private void checkPeriod(Period period, int years, int months, int days) {
+		assertEquals(period.getYears(), years);
+		assertEquals(period.getMonths(), months);
+		assertEquals(period.getDays(), days);
+	}
+}
