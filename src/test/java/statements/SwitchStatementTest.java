32e5405b86cd09ccb9d6e6cd5e78524a8d5b3334
ganchurin <eugeny.ganchurin@gmail.com>
2015-11-16 02:16:48 +0300

    Refactoring

--- src/test/java/statements/SwitchStatementTest.java
+++ src/test/java/statements/SwitchStatementTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class Switch {
+public class SwitchStatementTest {
 
 	@Test
 	public void testSwitch() {
--- src/test/java/statements/WhileLoopTest.java
+++ src/test/java/statements/WhileLoopTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class While {
+public class WhileLoopTest {
 
 	@Test
 	public void testWhile() {
