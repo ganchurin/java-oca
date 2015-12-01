bc1b379f6365ada719e95160aa570eef9a18e94d
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-01 03:19:54 +0300

    Introduced tests for lambdas

---
+++ src/test/java/core/lambda/LambdaTest.java
@@ -0,0 +1,36 @@
+package core.lambda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LambdaTest {
+
+	@Test
+	public void testLambda() {
+		Value v = new Value(1);
+		assertEquals(v.handle(x -> x + 1), 2);
+		assertEquals(v.handle((x) -> x + 1), 2);
+		assertEquals(v.handle((int x) -> x + 1), 2);
+
+		assertEquals(v.handle(x -> { return x + 1; }), 2);
+		assertEquals(v.handle((x) -> { return x + 1; }), 2);
+		assertEquals(v.handle((int x) -> {return x + 1; }), 2);
+	}
+}
+
+class Value {
+	private final int value;
+
+	Value(int value) {
+		this.value = value;
+	}
+
+	int handle(Handler handler) {
+		return handler.process(value);
+	}
+}
+
+interface Handler {
+	int process(int x);
+}
