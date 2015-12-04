6a8e16d94a71077aa9c4400e167759c782e8c657
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-04 03:37:03 +0300

    Refactoring: tests

--- src/main/java/core/primitives/DoublePrimitive.java
+++ src/main/java/core/primitives/DoublePrimitive.java
@@ -4,19 +4,19 @@ public class DoublePrimitive implements PrimitiveConstants {
 
 	double d1 = 100;
 
-	double d2 = BYTE;    // OK
+	double d2 = BYTE;
 
-	double d3 = SHORT;    // OK
+	double d3 = SHORT;
 
-	double d4 = INT;    // OK
+	double d4 = INT;
 
-	double d5 = LONG;    // OK
+	double d5 = LONG;
 
-	double d6 = FLOAT;    // OK
+	double d6 = FLOAT;
 
-	double d7 = DOUBLE;    // OK
+	double d7 = DOUBLE;
 
-	double d9 = CHAR;    // OK
+	double d9 = CHAR;
 
 	// COMPILE ERROR: incompatible types: boolean cannot be converted to double
 	// double d8 = BOOLEAN;
