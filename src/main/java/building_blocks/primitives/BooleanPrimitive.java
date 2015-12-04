66de31707db30b285e7aa80d3f016e4049ecc1e7
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-04 03:31:40 +0300

    Refactoring: tests

---
+++ src/main/java/building_blocks/primitives/BooleanPrimitive.java
@@ -0,0 +1,34 @@
+package building_blocks.primitives;
+
+public class BooleanPrimitive implements PrimitiveConstants {
+
+	// Assignments below do not compile...
+
+	// COMPILE ERROR: incompatible types: int cannot be converted to boolean
+	// boolean b1 = 100;
+
+	// COMPILE ERROR: incompatible types: byte cannot be converted to boolean
+	// boolean b2 = BYTE;
+
+	// COMPILE ERROR: incompatible types: short cannot be converted to boolean
+	// boolean b3 = SHORT;
+
+	// COMPILE ERROR: incompatible types: int cannot be converted to boolean
+	// boolean b4 = INT;
+
+	// COMPILE ERROR: incompatible types: long cannot be converted to boolean
+	// boolean b5 = LONG;
+
+	// COMPILE ERROR: incompatible types: float cannot be converted to boolean
+	// boolean b6 = FLOAT;
+
+	// COMPILE ERROR: incompatible types: double cannot be converted to boolean
+	// boolean b7 = DOUBLE;
+
+	// COMPILE ERROR: incompatible types: char cannot be converted to boolean
+	// boolean b9 = CHAR;
+
+	// ...except this
+
+	boolean b8 = BOOLEAN;    // OK
+}
