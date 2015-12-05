edcade480482ebaac4d5b5d2a029dd488880adc4
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-05 22:17:35 +0300

    Introduced test for List.toArray

--- src/test/java/core/collection/CollectionsTest.java
+++ src/test/java/core/collection/CollectionsTest.java
@@ -2,6 +2,7 @@ package core.collection;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -34,5 +35,28 @@ public class CollectionsTest {
 		assertEquals(Collections.binarySearch(capitals, "Madrid"), -3);
 		assertEquals(Collections.binarySearch(capitals, "Oslo"), -4);
 		assertEquals(Collections.binarySearch(capitals, "Stockholm"), -5);
+	}
+
+	@Test
+	public void toArray() {
+		List<String> capitals = new ArrayList<>();
+		capitals.add("Berlin");
+		capitals.add("Moscow");
+
+		Object[] objArray = capitals.toArray();
+
+		assertEquals(objArray[0], "Berlin");
+		assertEquals(objArray[1], "Moscow");
+
+		String[] strArray = capitals.toArray(new String[0]);
+
+		assertEquals(strArray[0], "Berlin");
+		assertEquals(strArray[1], "Moscow");
+
+		objArray[0] = "London";
+		strArray[1] = "Paris";
+
+		assertEquals(capitals.get(0), "Berlin");
+		assertEquals(capitals.get(1), "Moscow");
 	}
 }
