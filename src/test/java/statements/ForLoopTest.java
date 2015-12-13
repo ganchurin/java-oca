17b5c52a5c1b0f649cf3f36e8e9d8aba956dc5e8
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-14 02:18:30 +0300

    Refactoring: tests

--- src/test/java/statements/ForLoopTest.java
+++ src/test/java/statements/ForLoopTest.java
@@ -8,12 +8,66 @@ import static org.junit.Assert.assertTrue;
 public class ForLoopTest {
 
 	@Test
-	public void checkForLoop() {
-		int x, y = 0;
-		for (x = 0; x < 10; x++) {
-			y += y++;    // @todo extract into special example
+	public void checkLoop_01() {
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum += i;
 		}
-		assertTrue(x == 10);
+		assertTrue(sum == 45);
+	}
+
+	@Test
+	public void checkLoop_02() {
+		int sum = 0;
+		for (int i = 0, j = 0; i < 10 && j < 5; i++, j++) {
+			sum += i + j;
+		}
+		assertTrue(sum == 20);
+	}
+
+	@Test
+	public void checkLoop_03() {
+		int sum = 0;
+		int i = 0, j = 0;
+		for (; i < 10 && j < 5; i++, j++) {
+			sum += i + j;
+		}
+		assertTrue(sum == 20);
+	}
+
+	@Test
+	public void checkLoop_04() {
+		int sum = 0;
+		int i = 0, j = 0;
+		for (; i < 10 && j < 5; ) {
+			sum += i + j;
+			i++;
+			j++;
+		}
+		assertTrue(sum == 20);
+	}
+
+	@Test
+	public void checkLoop_05() {
+		int sum = 0;
+		int i = 0, j = 0;
+		for (; ; ) {
+			if (j == 5) {
+				break;
+			}
+			sum += i + j;
+			i++;
+			j++;
+		}
+		assertTrue(sum == 20);
+	}
+
+	@Test // @todo extract into special example
+	public void checkLoop_06() {
+		int y = 0;
+		for (int x = 0; x < 10; x++) {
+			y += y++;
+		}
 		assertTrue(y == 0);
 	}
 
