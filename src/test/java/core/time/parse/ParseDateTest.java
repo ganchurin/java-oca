ddef82843d7aa1a350adab06478b9845b3fcfb79
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-15 02:07:29 +0300

    New tests for parsing LocalDate

--- src/test/java/core/time/parse/ParseDateTest.java
+++ src/test/java/core/time/parse/ParseDateTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,7 +13,26 @@ public class ParseDateTest {
 
 	@Test
 	public void parseDate() {
-		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
-		assertEquals(LocalDate.parse("2015-10-30", formatter), LocalDate.of(2015, Month.OCTOBER, 30));
+		LocalDate expected = LocalDate.of(2015, Month.OCTOBER, 30);
+		assertEquals(LocalDate.parse("2015-10-30"), expected);
+	}
+
+	@Test
+	public void parseDateWithISOFormatter() {
+		LocalDate expected = LocalDate.of(2015, Month.OCTOBER, 30);
+		DateTimeFormatter isoFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+		assertEquals(LocalDate.parse("2015-10-30", isoFormatter), expected);
+	}
+
+	@Test
+	public void parseDateWithCustomFormatter() {
+		LocalDate expected = LocalDate.of(2015, Month.OCTOBER, 30);
+		DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("yyyy-dd-MM");
+		assertEquals(LocalDate.parse("2015-30-10", customFormatter), expected);
+	}
+
+	@Test(expected = DateTimeParseException.class)
+	public void parseWrongFormatDate() {
+		LocalDate.parse("2015-00-00");
 	}
 }
