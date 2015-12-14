4403d4aa12ad424ec3bbe10146904c2298a5023e
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-15 01:43:37 +0300

    refactoring - tests

--- src/test/java/core/time/date/LocalDateTest.java
+++ src/test/java/core/time/date/LocalDateTest.java
@@ -12,19 +12,19 @@ public class LocalDateTest {
 	@Test
 	public void createCurrentDate() {
 		// Use static method 'now' to create new date object with current date
-		LocalDate date = LocalDate.now();
+		LocalDate.now();
 	}
 
 	@Test
 	public void createCustomDate() {
 		// Use static method 'of' to create new custom date object
 		// Set year, month index and day of month
-		LocalDate d1 = LocalDate.of(2015, 10, 30);
-		checkDate(d1, 2015, Month.OCTOBER, 30);
+		LocalDate date1 = LocalDate.of(2015, 10, 30);
+		checkDate(date1, 2015, Month.OCTOBER, 30);
 
 		// Set year, month and day of month
-		LocalDate d2 = LocalDate.of(2015, Month.OCTOBER, 30);
-		checkDate(d2, 2015, Month.OCTOBER, 30);
+		LocalDate date2 = LocalDate.of(2015, Month.OCTOBER, 30);
+		checkDate(date2, 2015, Month.OCTOBER, 30);
 	}
 
 	private void checkDate(LocalDate date, int year, Month month, int day) {
@@ -37,10 +37,10 @@ public class LocalDateTest {
 	public void checkImmutability() {
 		// LocalDate objects are immutable
 		// Operations on LocalDate objects produce new LocalDate objects
-		LocalDate d = LocalDate.of(2015, 10, 30);
+		LocalDate date = LocalDate.of(2015, 10, 30);
 		// If you want to save result of operation on LocalDate object,
 		// then do not forget to assign new value to a reference
-		d.plusYears(10).minusMonths(10);
-		assertEquals(d, LocalDate.of(2015, 10, 30));
+		date.plusYears(10).minusMonths(10);
+		assertEquals(date, LocalDate.of(2015, 10, 30));
 	}
 }
--- src/test/java/core/time/time/LocalTimeTest.java
+++ src/test/java/core/time/time/LocalTimeTest.java
@@ -11,23 +11,23 @@ public class LocalTimeTest {
 	@Test
 	public void createCurrentTime() {
 		// Use static method 'now' to create new time object with current time
-		LocalTime time = LocalTime.now();
+		LocalTime.now();
 	}
 
 	@Test
 	public void createCustomTime() {
 		// Use static method 'of' to create new custom time object
 		// Set hours and minutes
-		LocalTime t1 = LocalTime.of(10, 30);
-		checkTime(t1, 10, 30, 0, 0);
+		LocalTime time1 = LocalTime.of(10, 30);
+		checkTime(time1, 10, 30, 0, 0);
 
 		// Set hours, minutes and seconds
-		LocalTime t2 = LocalTime.of(10, 30, 15);
-		checkTime(t2, 10, 30, 15, 0);
+		LocalTime time2 = LocalTime.of(10, 30, 15);
+		checkTime(time2, 10, 30, 15, 0);
 
 		// Set hours, minutes, seconds and nanoseconds
-		LocalTime t3 = LocalTime.of(10, 30, 15, 1000);
-		checkTime(t3, 10, 30, 15, 1000);
+		LocalTime time3 = LocalTime.of(10, 30, 15, 1000);
+		checkTime(time3, 10, 30, 15, 1000);
 	}
 
 	private void checkTime(LocalTime time, int hour, int minute, int second, long nano) {
@@ -41,10 +41,10 @@ public class LocalTimeTest {
 	public void checkImmutability() {
 		// LocalTime objects are immutable
 		// Operations on LocalTime objects produce new LocalTime objects
-		LocalTime t = LocalTime.of(10, 30);
+		LocalTime time = LocalTime.of(10, 30);
 		// If you want to save result of operation on LocalTime object,
 		// then do not forget to assign new value to a reference
-		t.plusHours(10).minusMinutes(10);
-		assertEquals(t, LocalTime.of(10, 30));
+		time.plusHours(10).minusMinutes(10);
+		assertEquals(time, LocalTime.of(10, 30));
 	}
 }
