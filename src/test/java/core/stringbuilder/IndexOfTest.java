33893c0bc96508095af88dca9db8aae41ae767e7
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-15 01:51:18 +0300

    refactoring - tests

--- src/test/java/core/stringbuilder/IndexOfTest.java
+++ src/test/java/core/stringbuilder/IndexOfTest.java
@@ -9,6 +9,7 @@ public class IndexOfTest {
 	@Test
 	public void checkIndexOfString() {
 		StringBuilder builder = new StringBuilder("123");
+
 		assertTrue(builder.indexOf("1") == 0);
 		assertTrue(builder.indexOf("2") == 1);
 		assertTrue(builder.indexOf("3") == 2);
@@ -18,6 +19,7 @@ public class IndexOfTest {
 	@Test
 	public void checkIndexOfStringFromIndex() {
 		StringBuilder builder = new StringBuilder("123");
+
 		assertTrue(builder.indexOf("1", 1) == -1);
 		assertTrue(builder.indexOf("2", 1) == 1);
 		assertTrue(builder.indexOf("3", 1) == 2);
