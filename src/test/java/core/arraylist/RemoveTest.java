2394be1b0a44399c468ba3508229b4915a2c6e6c
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-16 02:47:11 +0300

    Refactoring: tests

--- src/test/java/core/arraylist/RemoveTest.java
+++ src/test/java/core/arraylist/RemoveTest.java
@@ -5,10 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
 public class RemoveTest {
@@ -33,7 +33,7 @@ public class RemoveTest {
 		// The first matched element will be removed
 		// Returns true if element was removed, otherwise - false
 		assertEquals(list.remove("AAPL"), true);
-		assertEquals(list, asList("YNDX", "AAPL"));
+		assertEquals(list, Arrays.asList("YNDX", "AAPL"));
 	}
 
 	@Test
@@ -41,7 +41,7 @@ public class RemoveTest {
 		// Provide an integer to remove element by index
 		// Returns element previously at specified position
 		assertEquals(list.remove(0), "AAPL");
-		assertEquals(list, asList("YNDX", "AAPL"));
+		assertEquals(list, Arrays.asList("YNDX", "AAPL"));
 	}
 
 	@Test(expected = IndexOutOfBoundsException.class)
@@ -57,6 +57,6 @@ public class RemoveTest {
 	@Test
 	public void removeIfTest() {
 		list.removeIf(x -> x.startsWith("A"));
-		assertEquals(list, singletonList("YNDX"));
+		assertEquals(list, Collections.singletonList("YNDX"));
 	}
 }
--- src/test/java/core/arraylist/SetTest.java
+++ src/test/java/core/arraylist/SetTest.java
@@ -4,9 +4,9 @@ import org.junit.After;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
 public class SetTest {
@@ -21,12 +21,12 @@ public class SetTest {
 	@Test
 	public void setTest() {
 		list.add("AAPL");
-		assertEquals(list, singletonList("AAPL"));
+		assertEquals(list, Collections.singletonList("AAPL"));
 
 		// Provide index and element to set
 		// Returns element previously at specified position
 		assertEquals(list.set(0, "YNDX"), "AAPL");
-		assertEquals(list, singletonList("YNDX"));
+		assertEquals(list, Collections.singletonList("YNDX"));
 	}
 
 	@Test(expected = IndexOutOfBoundsException.class)
