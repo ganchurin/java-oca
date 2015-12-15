7fe220dd2023a860a7472b6d8c9ede6d6d1029b7
ganchurin <eugeny.ganchurin@gmail.com>
2015-12-15 03:05:07 +0300

    New tests for LocalTime

--- src/test/java/core/time/parse/ParseTimeTest.java
+++ src/test/java/core/time/parse/ParseTimeTest.java
@@ -11,7 +11,16 @@ public class ParseTimeTest {
 
 	@Test
 	public void parseTime() {
+		assertEquals(LocalTime.parse("10:30"), LocalTime.of(10, 30, 0));
+		assertEquals(LocalTime.parse("10:30:15"), LocalTime.of(10, 30, 15));
+		assertEquals(LocalTime.parse("10:30:15.500"), LocalTime.of(10, 30, 15, 500000000));
+	}
+
+	@Test
+	public void parseTimeWithISOFormatter() {
 		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
+		assertEquals(LocalTime.parse("10:30", formatter), LocalTime.of(10, 30, 0));
 		assertEquals(LocalTime.parse("10:30:15", formatter), LocalTime.of(10, 30, 15));
+		assertEquals(LocalTime.parse("10:30:15.500", formatter), LocalTime.of(10, 30, 15, 500000000));
 	}
 }
